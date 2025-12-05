0c7c1af5c3eebdbc718c81b8bed918286f19211a
Jacob Malone <jakethejrm@gmail.com>
2025-12-05 13:25:14 -0700

    Day 4 done

---
+++ 4/day4_b.py
@@ -0,0 +1,60 @@
+import subprocess
+
+grid = []
+accessible = 0
+
+with open("input") as f:
+    for line in f:
+        line = line.strip()
+        row = []
+        for c in line:
+            row.append(c)
+        grid.append(row)
+
+while True:
+    removed = []
+    for row_num in range(len(grid)):
+        for col_num in range(len(grid[row_num])):
+            if grid[row_num][col_num] == ".":
+                continue
+        
+            # c is @
+            num_around = 0
+            if col_num > 0:
+                if grid[row_num][col_num-1] == "@":
+                    num_around += 1
+                if row_num > 0:
+                    if grid[row_num-1][col_num-1] == "@":
+                        num_around += 1
+                    
+            if col_num < len(grid[0]) - 1:
+                if grid[row_num][col_num+1] == "@":
+                    num_around += 1
+                if row_num < len(grid) - 1:
+                    if grid[row_num + 1][col_num + 1] == "@":
+                        num_around += 1
+            if row_num > 0:
+                if grid[row_num - 1][col_num] == "@":
+                    num_around += 1
+                if col_num < len(grid[0]) - 1:
+                    if grid[row_num - 1][col_num + 1] == "@":
+                        num_around += 1
+            if row_num < len(grid) - 1:
+                if grid[row_num + 1][col_num] == "@":
+                    num_around += 1
+                if col_num > 0:
+                    if grid[row_num + 1][col_num - 1] == "@":
+                        num_around += 1
+
+            if num_around < 4:
+                accessible += 1
+                removed.append((row_num, col_num))
+    if len(removed) == 0:
+        break
+    else:
+        for t in removed:
+            grid[t[0]][t[1]] = "."
+
+answer = str(accessible)
+subprocess.run("pbcopy", text=True, input=answer)
+print(answer, "(copied to clipboard)")
--- template.py
+++ template.py
@@ -1,5 +1,8 @@
 import subprocess
 
+with open("input") as f:
+    pass
+
 answer = ""
 subprocess.run("pbcopy", text=True, input=answer)
 print(answer, "(copied to clipboard)")
