65fcc3d91df3bc9cbda884b05dd56b53d10ec1cf
Jacob Malone <jakethejrm@gmail.com>
2025-12-05 10:24:06 -0700

    day 2

---
+++ 2/day2_a.py
@@ -0,0 +1,19 @@
+invalid = []
+
+with open("input") as f:
+    ranges = f.read().split(",")
+    for r in ranges:
+        b_and_e = r.split("-")
+        b = int(b_and_e[0])
+        e = int(b_and_e[1])
+        for i in range(b,e+1):
+            s = str(i)
+            if len(s) % 2 != 0:
+                continue
+            
+            middle = len(s) // 2
+            if s[:middle] == s[middle:]:
+                invalid.append(i)
+
+print(invalid)
+print(sum(invalid))
---
+++ 2/day2_b.py
@@ -0,0 +1,16 @@
+invalid = []
+
+with open("input") as f:
+    ranges = f.read().split(",")
+    for r in ranges:
+        b_and_e = r.split("-")
+        b = int(b_and_e[0])
+        e = int(b_and_e[1])
+        for i in range(b,e+1):
+            s = str(i)
+            
+            if (s + s).index(s, 1) != len(s):
+                invalid.append(i)
+
+print(invalid)
+print(sum(invalid))
